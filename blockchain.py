9df64079b8a9051c63d72f59b95b92c55358a6b5
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2021-12-18 14:14:58 +0700

    add chain validation method for nodes sync

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 
 from time import time
+from urllib.parse import urlparse
 
 
 class Blockchain(object):
@@ -9,6 +10,7 @@ class Blockchain(object):
     difficulty_target = "0000"
 
     def __init__(self):
+        self.nodes = set()
         self.chain = []
         self.current_transactions = []
 
@@ -17,6 +19,35 @@ class Blockchain(object):
             genesis_hash,
             self.proof_of_work(0, genesis_hash, [])
         )
+
+    def add_node(self, address):
+        parse_url = urlparse(address)
+        self.nodes.add(parse_url.netloc)
+
+        print(parse_url.netloc)
+
+    def chain_validation(self, chain):
+        previous_block = chain[0]
+
+        index = 1
+        while (index < len(chain)):
+            block = chain[index]
+
+            if (block['hash_of_previous_block'] != self.hash_block(previous_block)):
+                return False
+
+            if not self.nonce_validation(
+                index=index,
+                hash_of_previous_block=block['hash_of_previous_block'],
+                transaction=block['transactions'],
+                nonce=block['nonce']
+            ):
+                return False
+
+            previous_block = block
+            index += 1
+
+        return True
 
     def hash_block(self, block):
         encoded_block = json.dumps(block, sort_keys=False).encode()
